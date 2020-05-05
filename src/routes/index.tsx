5ff96d90d2b4887e068d704972583878f1fe31d0
Ednilton Junior <jrjunior_cg@live.com>
2020-05-05 00:42:16 -0300

    commits

---
+++ src/routes/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard';
+import Logon from '../pages/Logon';
+import Home from '../pages/Home';
+
+import Merchant from '../pages/Merchant';
+
+import Profile from '../pages/Profile';
+import Suppliers from '../pages/Suppliers';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/auth" component={Logon} />
+
+    <Route path="/profile" component={Profile} />
+
+    <Route path="/suppliers" component={Suppliers} />
+    <Route path="/home" component={Home} />
+    <Route path="/merchant" component={Merchant} />
+  </Switch>
+);
+
+export default Routes;
